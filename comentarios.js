43be28d973311bfe570eafd73bc2146e837c5255
Roberto Argañaraz <gustavoargentina2000@gmail.com>
2023-01-31 02:02:02 -0300

    prueba

---
+++ comentarios.js
@@ -0,0 +1,10 @@
+// con doble barra hacemos comentario de 1 linea
+
+/** */    //con /** */ hacemos comentarios de varias lineas
+/**los comentarios deben ser importantes por ej en el if no podemos comentar
+ * "acá valido si tal cosa"ya que ya sabemos que if valida.. 
+ * el comentario correcto seria: "acá valido xq el cliente pidio tal cosa"
+ */
+if (true){
+    console.log("Hola");
+}
